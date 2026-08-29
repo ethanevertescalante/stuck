90da35cc216149726cd5aa26c2d0f513fe5d46b3
ethanescalante <ethan65g@gmail.com>
2026-08-28 23:28:22 -0700

    added toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Jersey_15, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toast"
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -29,6 +30,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <Toaster />
     </html>
   );
 }
