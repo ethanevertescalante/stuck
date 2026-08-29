5e5da04f0bd4848b73bdf19d24de7a3b59525b0e
ethanescalante <ethan65g@gmail.com>
2026-08-28 21:00:32 -0700

    added POST and GET functions for backend sticky

--- app/api/sticky/route.ts
+++ app/api/sticky/route.ts
@@ -1,20 +1,72 @@
 import {NextRequest, NextResponse} from "next/server";
+import { prisma } from "@/lib/prisma"
 import * as z from "zod";
-const stickySchema = z.object({
+import {requireUser} from "@/lib/requireUser";
+import {serverError} from "@/lib/serverError";
+import { StickyType } from "@/app/generated/prisma/enums";
+
+const createStickySchema = z.object({
     stickyName: z.string().min(1, "Please enter a name for the sticky"),
-    stickyContent: z.optional(z.string()),
-    stickyDate: z.optional(z.date()),
-    stickyType: z.string().uppercase()
+    stickyContent: z.string().optional(),
+    stickyDate: z.date().optional(),
+    stickyType: z.enum(StickyType),
 })
 
+export async function GET () {
+    try{
+       const user = await requireUser();
 
-function POST (req: NextRequest, res: NextResponse) {
+       const stickies = await prisma.sticky.findMany({
+           where: {
+               userId: user.id,
+           }
+       })
 
+       return NextResponse.json({
+           data: stickies,
+       })
+
+    }catch(error){
+        const message = "GET /api/sticky failed:"
+        serverError(message, error);
+    }
 }
 
-function GET () {
+async function POST (request: NextRequest) {
+    try{
+        const user = await requireUser();
 
+        const body = await request.json();
+        const result = createStickySchema.safeParse(body);
+        if(!result.success){
+            return NextResponse.json(
+                {error: "Invalid sticky data"},
+                {status: 400}
+            )
+        }
+
+        const {stickyName, stickyContent, stickyType, stickyDate } = result.data;
+        const sticky = await prisma.sticky.create({
+            data:{
+                stickyName: stickyName,
+                stickyContent: stickyContent,
+                stickyType: stickyType,
+                stickyDueDate: stickyDate,
+                userId: user.id,
+            }
+        })
+
+        return NextResponse.json(
+            {data: sticky},
+            {status: 201}
+        )
+
+    }catch(error){
+        const message = "POST /api/sticky failed:"
+        serverError(message, error);
+    }
 }
+
 
 function PUT (req: NextRequest, res: NextResponse) {
 
